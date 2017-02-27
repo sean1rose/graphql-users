0ad29521668c1a7767d43ff3a1b15158398f9fe6
Sean Rose <sean1rose@gmail.com>
2017-02-27 00:11:05 -0800

    begin work on user graphql schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,20 @@
-// tells graphql what our data looks like, what properties each obj has, and how each obj is related to each other
+// SCHEMA tells graphql exactly what our data looks like, what properties each obj has, and how each obj is related to each other
+
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} = graphql;
+
+// represents User obj, and what the obj will look like -> defines data and r/s's
+  // fields prop: tells graphql about all the diff props a user has
+  // tell graphql the type of data (number, string, array) each field is 
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstname: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+})
